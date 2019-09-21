05ddb93e8dd57017e60ee3a9fc0ddc28652a1315
danbaziak <danielbaziak@gmail.com>
2019-09-21 12:22:37 +0200

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+F
+app/proguard-rules.pro,9/e/9e08934d811afe28fbc77aaa3c0d747b94348db9
+A
+gradle.properties,2/a/2afbb999f001938c88fa43fc2ef52abf0f8213e4
+ƒ
+Sapp/src/androidTest/java/com/example/workout_rival_app/ExampleInstrumentedTest.java,5/d/5d095c532fd0b9959f39148b099adbe19477cc92
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+P
+ app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
+q
+Aapp/src/main/java/com/example/workout_rival_app/MainActivity.java,2/5/25630548a03ea157efc8bd00c8022120a134639b
+r
+Bapp/src/main/java/com/example/workout_rival_app/WelcomeScreen.java,f/a/fa267372f4e0d7c2bfcec1c590832b15867db355
---
+++ app/src/main/java/com/example/workout_rival_app/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.workout_rival_app;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+	private static final String SIMPLE_GET_URL = "http://192.168.0.101:8080/workoutRival"; //hardcoded my IP
+	private String message = "";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		toolbar.setBackgroundColor(Color.RED);
+
+		FloatingActionButton fab = findViewById(R.id.fab);
+		fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Snackbar.make(view, getSimpleRequest(), Snackbar.LENGTH_LONG)
+					.setAction("Action", null).show();
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		if (id == R.id.Quit) {
+			this.finish();
+			System.exit(0);
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	private String getSimpleRequest() {
+		RequestQueue queue = Volley.newRequestQueue(this);
+		final StringRequest getRequest = new StringRequest(Request.Method.GET, SIMPLE_GET_URL,
+			new Response.Listener<String>() {
+				@Override
+				public void onResponse(String response) {
+					setMessage(response);
+					Log.d("Response", response);
+				}
+			},
+			new Response.ErrorListener() {
+				@Override
+				public void onErrorResponse(VolleyError error) {
+					Log.d("Error.Response", error.toString());
+				}
+			}
+		);
+		queue.add(getRequest);
+		if (message.isEmpty()) {
+			return "\"This will be coming from backend service !\"";
+		}
+		return this.message;
+	}
+
+	private void setMessage(String message) {
+		this.message = message;
+	}
+}
